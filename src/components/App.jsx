69f27a8d229373d1b5217f086d2a0dd875d3e9b8
Evgeniy-Nazarov <perfectyam@gmail.com>
2023-03-19 01:58:19 -0400

    update2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
-import { User } from "./Profile/Profile";
-import users from "./Profile/users.json"
-import dataInformation from "./Statistics/data.json";
-import { UploadInfo } from "./Statistics/Statistics";
-import friends from "./FriendList/FriendsCatalog.json";
-import { FriendsItems } from "./FriendList/FriendList";
-import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
-import transactions from "./TransactionHistory/transactions.json";
-import { UserStyled } from "./Profile/Profile.styled";
+import { User } from "../Profile/Profile";
+import users from "../Profile/users.json"
+import dataInformation from "../Statistics/data.json";
+import { UploadInfo } from "../Statistics/Statistics";
+import friends from "../FriendList/FriendsCatalog.json";
+import { FriendsItems } from "../FriendList/FriendList";
+import { TransactionHistory } from "../TransactionHistory/TransactionHistory";
+import transactions from "../TransactionHistory/transactions.json";
+import { UserStyled } from "../Profile/Profile.styled";
 
 
 
